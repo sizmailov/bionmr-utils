887bbe64a07379789f7f3ab284075507244f0920
OOLebedenko <oolebedenko@gmail.com>
2019-10-25 17:02:23 +0300

    Initial add calc.py

---
+++ bionmr_utils/md/process/calc.py
@@ -0,0 +1,82 @@
+import numpy as np
+import pandas as pd 
+import os
+from typing import Tuple
+from pyxmolpp2.geometry import calc_autocorr_order_2
+
+
+def calc_autocorr(vectors: Dict[tuple, float],
+                  calc_autocorr_func=calc_autocorr_order_2
+                  ) :
+    """
+    Get auto-correlation from trajectory
+
+    :param vectors: dict of (rid, aname): VectorXYZ
+    :return dict of (rid, aname): auto-correlation
+    """
+    autocorr = {(rid, aname): calc_autocorr_func(vector)
+                for (rid, aname), vector in vectors.items()
+                }
+
+    return autocorr
+
+
+def calc_mean_square_displacement(time: np.array,
+                                  mass_centers: np.array,
+                                  lag_index: np.array
+                                  ) -> Tuple[np.array, np.array]:
+    """
+    :param time: time-points between mass_centers
+    :param mass_centers: N*3 array [x,y,z] columns
+    :param lag_index: int array of time lags between mass_centers
+    :return: tuple of two arrays (time_lag, msd)
+    """
+
+    assert time.shape[0] == mass_centers.shape[0]
+    assert mass_centers.shape[1] == 3
+
+    time_lags = []
+    msds = []
+    for int_lag in lag_index:
+        lag_time = time[int_lag]
+        msd = ((mass_centers[int_lag:] - mass_centers[:-int_lag])**2).sum(axis=1).mean()
+        time_lags.append(lag_time)
+        msds.append(msd)
+
+    return np.array(time_lags), np.array(msds)
+
+
+def calc_mean_square_displacement_by_axes(time: np.array,
+                                  mass_centers: np.array,
+                                  lag_index: np.array
+                                  ) -> Tuple[np.array, np.array]:
+    """
+    :param time: time-points between mass_centers
+    :param mass_centers: N*3 array [x,y,z] columns
+    :param lag_index: int array of time lags between mass_centers
+    :return: tuple of fore arrays (time_lag, msd_x, msd_y, msd_z)
+    """
+
+    assert time.shape[0] == mass_centers.shape[0]
+    assert mass_centers.shape[1] == 3
+
+    mass_centers_x = mass_centers[:, 0]
+    mass_centers_y = mass_centers[:, 1]
+    mass_centers_z = mass_centers[:, 2]
+
+    time_lags = []
+    msds_x = []
+    msds_y = []
+    msds_z = []
+
+    for int_lag in lag_index:
+        lag_time = time[int_lag]
+        msd_x = ((mass_centers_x[int_lag:] - mass_centers_x[:-int_lag])**2).mean()
+        msd_y = ((mass_centers_y[int_lag:] - mass_centers_y[:-int_lag])**2).mean()
+        msd_z = ((mass_centers_z[int_lag:] - mass_centers_z[:-int_lag])**2).mean()
+        time_lags.append(lag_time)
+        msds_x.append(msd_x)
+        msds_y.append(msd_y)
+        msds_z.append(msd_z)
+
+    return np.array(time_lags), np.array(msds_x),  np.array(msds_y), np.array(msds_z)
