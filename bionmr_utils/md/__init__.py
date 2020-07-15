1b00246c5a6a9d9749eed661e50266807e0267db
OOLebedenko <oolebedenko@gmail.com>
2020-07-15 18:07:42 +0300

    temporary commented md_init_py for tests run

--- bionmr_utils/md/__init__.py
+++ bionmr_utils/md/__init__.py
@@ -1,83 +1,83 @@
-from pyxmolpp2.polymer import (  # noqa: F401
-    aId,
-    aName,
-    Atom,
-    AtomName,
-    AtomPredicate,
-    AtomSelection,
-    Chain,
-    ChainName,
-    ChainPredicate,
-    ChainSelection,
-    cIndex,
-    cName,
-    Frame,
-    Residue,
-    ResidueId,
-    ResidueInsertionCode,
-    ResidueName,
-    ResiduePredicate,
-    ResidueSelection,
-    rId,
-    rName,
-    TorsionAngle,
-    TorsionAngleFactory
-)
+# from pyxmolpp2.polymer import (  # noqa: F401
+#     aId,
+#     aName,
+#     Atom,
+#     AtomName,
+#     AtomPredicate,
+#     AtomSelection,
+#     Chain,
+#     ChainName,
+#     ChainPredicate,
+#     ChainSelection,
+#     cIndex,
+#     cName,
+#     Frame,
+#     Residue,
+#     ResidueId,
+#     ResidueInsertionCode,
+#     ResidueName,
+#     ResiduePredicate,
+#     ResidueSelection,
+#     rId,
+#     rName,
+#     TorsionAngle,
+#     TorsionAngleFactory
+# )
 
-from pyxmolpp2.crystal import (  # noqa: F401
-    BestShiftFinder,
-    LatticeVectors
-)
+# from pyxmolpp2.crystal import (  # noqa: F401
+#     BestShiftFinder,
+#     LatticeVectors
+# )
 
-from pyxmolpp2.geometry import (  # noqa: F401
-    angle as calc_angle,
-    AngleValue,
-    calc_alignment,
-    calc_autocorr_order_2,
-    calc_autocorr_order_2_PRE,
-    calc_geom_center,
-    calc_inertia_tensor,
-    calc_mass_center,
-    calc_rmsd,
-    calc_sasa,
-    cos,
-    Degrees,
-    degrees_to_radians,
-    dihedral_angle as calc_dihedral_angle,
-    distance,
-    distance2,
-    fabs,
-    Radians,
-    radians_to_degrees,
-    Rotation3d,
-    sin,
-    tan,
-    Transformation3d,
-    Translation3d,
-    UniformScale3d,
-    VectorXYZ,
-    XYZ,
-)
+# from pyxmolpp2.geometry import (  # noqa: F401
+#     angle as calc_angle,
+#     AngleValue,
+#     calc_alignment,
+#     calc_autocorr_order_2,
+#     calc_autocorr_order_2_PRE,
+#     calc_geom_center,
+#     calc_inertia_tensor,
+#     calc_mass_center,
+#     calc_rmsd,
+#     calc_sasa,
+#     cos,
+#     Degrees,
+#     degrees_to_radians,
+#     dihedral_angle as calc_dihedral_angle,
+#     distance,
+#     distance2,
+#     fabs,
+#     Radians,
+#     radians_to_degrees,
+#     Rotation3d,
+#     sin,
+#     tan,
+#     Transformation3d,
+#     Translation3d,
+#     UniformScale3d,
+#     VectorXYZ,
+#     XYZ,
+# )
 
-from pyxmolpp2.pdb import (  # noqa: F401
-    AlteredPdbRecords,
-    FieldName as PdbFieldName,
-    PdbFile,
-    RecordName as PdbRecordName,
-    StandardPdbRecords
-)
+# from pyxmolpp2.pdb import (  # noqa: F401
+#     AlteredPdbRecords,
+#     FieldName as PdbFieldName,
+#     PdbFile,
+#     RecordName as PdbRecordName,
+#     StandardPdbRecords
+# )
 
-from pyxmolpp2.trajectory import (  # noqa: F401
-    Trajectory,
-    TrajectoryPortion
-)
+# from pyxmolpp2.trajectory import (  # noqa: F401
+#     Trajectory,
+#     TrajectoryPortion
+# )
 
-from pyxmolpp2.trjtool import (  # noqa: F401
-    DatFile
-)
+# from pyxmolpp2.trjtool import (  # noqa: F401
+#     DatFile
+# )
 
-from pyxmolpp2.amber import (  # noqa: F401
-    NetCDFTrajectoryFile
-)
+# from pyxmolpp2.amber import (  # noqa: F401
+#     NetCDFTrajectoryFile
+# )
 
-from .shortcuts import traj_from_dir  # noqa: F401
+# from .shortcuts import traj_from_dir  # noqa: F401
