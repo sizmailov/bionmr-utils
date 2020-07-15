3d6176563213f0e3d54e279ac8a50d8ccb1daa8b
OOLebedenko <oolebedenko@gmail.com>
2020-07-15 18:05:46 +0300

    upgrade_tests_with_pyxmolpp2_v1

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -1,5 +1,5 @@
-from bionmr_utils.md import PdbFile
-from bionmr_utils.md.process.select import get_mtsl_vectors, get_NH_vectors, get_methyl_vectors
+from pyxmolpp2 import PdbFile
+from bionmr_utils.md.process.select import get_mtsl_selection, get_NH_selection, get_methyl_selection
 from bionmr_utils.md.process.extract import extract_vectors
 import numpy as np
 
@@ -12,45 +12,44 @@ def test_extract_NH_vectors():
     H_1 = np.array([-9.909, -9.029, 6.241])
     H_56 = np.array([8.943, 5.351, 2.161])
 
-    NH_1 = [N_1 - H_1]
-    NH_56 = [N_56 - H_56]
+    NH_1 = N_1 - H_1
+    NH_56 = N_56 - H_56
 
-    frame = PdbFile(path).get_frames()
-    vectors = extract_vectors(frame, get_vectors=get_NH_vectors)
+    frames = PdbFile(path).frames()
+    vectors_dict = list(extract_vectors(frames, get_selection=get_NH_selection))[0]
 
-    assert (len(vectors)) == 55
+    assert (len(vectors_dict)) == 55
 
-    keys = sorted(list(vectors.keys()))
-    np.testing.assert_allclose(vectors[keys[0]].to_numpy(), NH_1)
-    np.testing.assert_allclose(vectors[keys[-1]].to_numpy(), NH_56)
+    keys = sorted(list(vectors_dict.keys()))
+    np.testing.assert_allclose(vectors_dict[keys[0]], NH_1)
+    np.testing.assert_allclose(vectors_dict[keys[-1]], NH_56)
 
-    assert str(keys[0][0]) == '2'
-    assert str(keys[-1][0]) == '56'
+    assert keys[0][0] == 2
+    assert keys[-1][0] == 56
 
 
 def test_extract_methyl_vectors():
     path = "tests_dataset/amber/GB1_F30C_MTSL/box.pdb"
-    CD1_LEU2 = np.array([0.504, 0.021, 1.995])
+    CG2_THR2 = np.array([-8.902, -5.237, 7.570])
     CG2_THR55 = np.array([10.884, 2.547, 3.218])
 
-    HD11_LEU2 = np.array([-0.010, -0.523, 1.202])
+    HG21_THR2 = np.array([-9.476, -4.531, 6.970])
     HG21_THR55 = np.array([10.525, 3.199, 4.014])
 
-    CH3_LEU2 = [CD1_LEU2 - HD11_LEU2]
-    CH3_THR55 = [CG2_THR55 - HG21_THR55]
+    CH3_THR2 = CG2_THR2 - HG21_THR2
+    CH3_THR55 = CG2_THR55 - HG21_THR55
 
-    frame = PdbFile(path).get_frames()
-    vectors = extract_vectors(frame, get_vectors=get_methyl_vectors)
+    frames = PdbFile(path).frames()
+    vectors_dict = list(extract_vectors(frames, get_selection=get_methyl_selection))[0]
 
-    assert (len(vectors)) == 33
+    assert (len(vectors_dict)) == 33
 
-    keys = sorted(list(vectors.keys()), key=lambda x: x[0])
+    keys = sorted(list(vectors_dict.keys()))
+    np.testing.assert_allclose(vectors_dict[keys[0]], CH3_THR2)
+    np.testing.assert_allclose(vectors_dict[keys[-1]], CH3_THR55)
 
-    np.testing.assert_allclose(vectors[keys[1]].to_numpy(), CH3_LEU2)
-    np.testing.assert_allclose(vectors[keys[-1]].to_numpy(), CH3_THR55)
-
-    assert str(keys[0][0]) == '2'
-    assert str(keys[-1][0]) == '55'
+    assert keys[0][0] == 2
+    assert keys[-1][0] == 55
 
 
 def test_extract_mtsl_vectors():
@@ -63,17 +62,16 @@ def test_extract_mtsl_vectors():
     H_1 = np.array([-9.909, -9.029, 6.241])
     H_56 = np.array([8.943, 5.351, 2.161])
 
-    mtsl_H_1 = [H_1 - electron]
-    mtsl_H_56 = [H_56 - electron]
+    mtsl_H_1 = electron - H_1
+    mtsl_H_56 = electron - H_56
 
-    frame = PdbFile(path).get_frames()
-    vectors = extract_vectors(frame, get_vectors=get_mtsl_vectors)
+    frame = PdbFile(path).frames()
+    vectors_dict = list(extract_vectors(frame, get_selection=get_mtsl_selection))[0]
+    assert (len(vectors_dict)) == 55
 
-    assert (len(vectors)) == 55
+    keys = sorted(list(vectors_dict.keys()))
+    np.testing.assert_allclose(vectors_dict[keys[0]], mtsl_H_1)
+    np.testing.assert_allclose(vectors_dict[keys[-1]], mtsl_H_56)
 
-    keys = sorted(list(vectors.keys()))
-    np.testing.assert_allclose(vectors[keys[0]].to_numpy(), mtsl_H_1)
-    np.testing.assert_allclose(vectors[keys[-1]].to_numpy(), mtsl_H_56)
-
-    assert str(keys[0][0]) == '2'
-    assert str(keys[-1][0]) == '56'
+    assert keys[0][0] == 2
+    assert keys[-1][0] == 56
